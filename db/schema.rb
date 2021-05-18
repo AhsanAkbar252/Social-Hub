170ba69e3099019ed2807ddccd427844122a14f8
Ahsan Akbar <ahsanakbar252@gmail.com>
2021-05-18 15:32:38 +0500

    deploying

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,30 @@
 
 ActiveRecord::Schema.define(version: 2021_03_20_153705) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "commentable_id"
@@ -19,6 +43,12 @@ ActiveRecord::Schema.define(version: 2021_03_20_153705) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "message"
   end
 
   create_table "friend_requests", force: :cascade do |t|
@@ -54,6 +84,58 @@ ActiveRecord::Schema.define(version: 2021_03_20_153705) do
     t.string "city"
     t.string "country"
     t.string "group_rules"
+  end
+
+  create_table "job_applications", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.string "qualification"
+    t.string "experience"
+    t.string "skills"
+    t.string "personalinfo"
+    t.integer "user_id"
+    t.integer "job_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "file_file_name"
+    t.string "file_content_type"
+    t.integer "file_file_size"
+    t.datetime "file_updated_at"
+    t.binary "file"
+    t.string "address"
+    t.string "city"
+    t.string "country"
+    t.string "experience_detail"
+    t.string "qualification_detail"
+    t.string "previous_company"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string "applicant_email"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "qualification"
+    t.string "experience"
+    t.string "salary"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "job_address"
+    t.string "job_city"
+    t.string "job_country"
+    t.string "package_detail"
+    t.string "employer_detail"
+    t.string "job_requirements"
+    t.string "employer_email"
+    t.string "job_type"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "page_followers", force: :cascade do |t|
@@ -171,6 +253,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_153705) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reactions", "posts"
   add_foreign_key "reactions", "users"
   add_foreign_key "room_messages", "rooms"
