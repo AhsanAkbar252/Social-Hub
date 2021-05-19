5d35dce8eb106d982a7cfa1e6d44904d6e2a717e
Ahsan Akbar <ahsanakbar252@gmail.com>
2021-05-19 11:55:16 +0500

    commit13

--- db/migrate/20210318143237_change_datatpe.rb
+++ db/migrate/20210318143237_change_datatpe.rb
@@ -1,5 +1,5 @@
 class ChangeDatatpe < ActiveRecord::Migration[6.0]
   def change
-    change_column :pages, :admin_id, :integer
+    change_column :pages, :admin_id, :integer, using: 'admin_id::integer'
   end
 end
